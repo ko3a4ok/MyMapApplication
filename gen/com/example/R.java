355f9d93e13c24b6bbd858e42ecc8b730f0f3b25
ko3a4ok <ko3a4ok.ua@gmail.com>
2012-02-14 11:10:35 +0200

    readme commit

--- gen/com/example/R.java
+++ gen/com/example/R.java
@@ -11,7 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int checkbox_empty=0x7f020000;
+        public static final int circle=0x7f020000;
     }
     public static final class id {
         public static final int map_id=0x7f050000;

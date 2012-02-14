301e50101891d52b4f22fc036d1bb3b8f2e4e7fc
ko3a4ok <ko3a4ok.ua@gmail.com>
2012-02-14 11:21:11 +0200

    third

--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -16,13 +16,13 @@ import java.util.*;
 public class MyActivity extends MapActivity {
     private MapView map;
     InterestingLocation interestingLocation;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         Drawable d = getResources().getDrawable(android.R.drawable.star_on);
-        d.setBounds(0,0,d.getIntrinsicWidth(), d.getIntrinsicHeight());
+        d.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
         interestingLocation = new InterestingLocation(d);
         map = (MapView) findViewById(R.id.map_id);
         map.setBuiltInZoomControls(true);
@@ -35,7 +35,7 @@ public class MyActivity extends MapActivity {
         map.getController().setZoom(15);
 
     }
-    
+
     public void onClick(View v) {
         GeoPoint gp = map.getMapCenter();
         interestingLocation.add(new OverlayItem(gp, "Fuck", "fuck " + Math.random()));
@@ -49,10 +49,11 @@ public class MyActivity extends MapActivity {
     class InterestingLocation extends ItemizedOverlay {
         private List<OverlayItem> location = new ArrayList<OverlayItem>();
         private Drawable marker;
+
         public InterestingLocation(Drawable d) {
             super(d);
             marker = d;
-            GeoPoint gp = new GeoPoint((int)(50.55358*1000000),(int)(30.510406*1000000));
+            GeoPoint gp = new GeoPoint((int) (50.55358 * 1000000), (int) (30.510406 * 1000000));
             location.add(new OverlayItem(gp, "Fuck", "fuck " + Math.random()));
             populate();
         }
@@ -76,7 +77,7 @@ public class MyActivity extends MapActivity {
             List<Point> ls = new ArrayList<Point>();
             Projection projection = map.getProjection();
             for (OverlayItem oi : location)
-            ls.add(projection.toPixels(oi.getPoint(), new Point()));
+                ls.add(projection.toPixels(oi.getPoint(), new Point()));
             return ls.size() > 3 ? orderedPoints(ls) : ls;
         }
 
@@ -84,7 +85,7 @@ public class MyActivity extends MapActivity {
             Point first = Collections.min(ls, new Comparator<Point>() {
                 @Override
                 public int compare(Point o1, Point o2) {
-                    if (o1.x < o2.x ) return -1;
+                    if (o1.x < o2.x) return -1;
                     if (o1.x == o2.y && o1.y < o2.y) return -1;
                     return Integer.valueOf(o1.y).compareTo(o2.y);
                 }
@@ -93,27 +94,25 @@ public class MyActivity extends MapActivity {
             res.add(first);
             Point current = null;
             Point prev = first;
-            while (current != first){
+            while (current != first) {
                 for (Point pt : ls)
                     if (pt != prev) current = pt;
-                
+
                 for (Point pt : ls)
-                    if (pt != prev && pt != current)
-                        if ((current.x-prev.x)*(pt.y-prev.y)-(current.y-prev.y)*(pt.x-prev.x) > 0)
-                            current = pt;
+                    if (pt != prev && pt != current) if ((current.x - prev.x) * (pt.y - prev.y) - (current.y - prev.y) * (pt.x - prev.x) > 0) current = pt;
                 res.add(current);
-                prev = current;                
+                prev = current;
             }
             return res;
         }
-        
+
         @Override
         public boolean draw(Canvas canvas, MapView mapView, boolean b, long l) {
             boolean r = super.draw(canvas, mapView, b, l);
             Paint p = new Paint();
             p.setAntiAlias(true);
             p.setColor(0x330000ff);
-            canvas.drawCircle(mapView.getWidth()/2, mapView.getHeight()/2, 10f, p);
+            canvas.drawCircle(mapView.getWidth() / 2, mapView.getHeight() / 2, 10f, p);
             Path path = new Path();
             List<Point> ls = getPoints();
 
@@ -125,33 +124,32 @@ public class MyActivity extends MapActivity {
             return r;
         }
 
-        private boolean   isPinch  =  false;
+        private boolean isPinch = false;
 
         @Override
-        public boolean onTap(GeoPoint p, MapView map){
-            if ( isPinch ){
+        public boolean onTap(GeoPoint p, MapView map) {
+            if (isPinch) {
                 return false;
-            }else{
-                if ( p!=null ){
+            } else {
+                if (p != null) {
                     add(new OverlayItem(p, "new", "new"));
-                    return true;  
-                }else{
-                    return false; 
+                    return true;
+                } else {
+                    return false;
                 }
             }
         }
 
         @Override
-        public boolean onTouchEvent(MotionEvent e, MapView mapView)
-        {
+        public boolean onTouchEvent(MotionEvent e, MapView mapView) {
             int fingers = e.getPointerCount();
-            if( e.getAction()==MotionEvent.ACTION_DOWN ){
-                isPinch=false;  // Touch DOWN, don't know if it's a pinch yet
+            if (e.getAction() == MotionEvent.ACTION_DOWN) {
+                isPinch = false;  // Touch DOWN, don't know if it's a pinch yet
             }
-            if( e.getAction()==MotionEvent.ACTION_MOVE && fingers==2 ){
-                isPinch=true;   // Two fingers, def a pinch
+            if (e.getAction() == MotionEvent.ACTION_MOVE && fingers == 2) {
+                isPinch = true;   // Two fingers, def a pinch
             }
-            return super.onTouchEvent(e,mapView);
+            return super.onTouchEvent(e, mapView);
         }
     }
 }
